a9161790925827b6d15e914308bb44cee04ccb1b
Dominique Hill <50179031+mzhill08@users.noreply.github.com>
2019-11-10 23:36:54 -0500

    Submitting my scene and files downloaded
    I added a scripts folder, art folder and prefab folder in the resource folder. I also downloaded a workbence asset pack which is in the art folder. I also made a scene with my name for testing purposes.

---
+++ reGUNstruction/Assets/Scripts/MoveTarget.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] wayPoints;
+    int currentWaypoint = 0;
+    Rigidbody rb;
+    [SerializeField]
+    float moveSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if(Vector3.Distance(transform.position, wayPoints[currentWaypoint].position) < .25f)
+        {
+            currentWaypoint += 1;
+            currentWaypoint = currentWaypoint % wayPoints.Length;
+        }
+        Vector3 _dir = (wayPoints[currentWaypoint].position - transform.position).normalized;
+        rb.MovePosition(transform.position + _dir * moveSpeed * Time.deltaTime);
+    }
+}
