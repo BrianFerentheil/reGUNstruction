211600bc4cb92247b24e1ac38fb04e703ecd4263
Chenfeng Zhang <akupaqi123@gmail.com>
2019-11-11 18:36:43 -0500

    Weapon funcion and particles

---
+++ reGUNstruction/Assets/Scripts/GunShooting/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed;
+
+
+    private void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    void Update()
+    {
+        this.GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+    }
+}
---
+++ reGUNstruction/Assets/Scripts/GunShooting/Weapon.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bullet;
+    public float fireRate;
+    public Transform spawnTransform;
+    public bool isFiring;
+    public float timer;
+    public bool raycast;
+    public GameObject rayCastHit;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (raycast)
+            {
+                isFiring = true;
+                RayCastFire();
+            }
+            else
+            {
+                isFiring = true;
+                Fire();
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            isFiring = false;
+        }
+    }
+
+    public void Fire()
+    {
+        if (!isFiring)
+        {
+            return;
+        }
+        else
+        {
+            if(timer >= fireRate)
+            {
+                timer = 0;
+                Instantiate(bullet, spawnTransform);
+            }
+        }
+    }
+
+    public void RayCastFire()
+    {
+        if (!isFiring)
+        {
+            return;
+        }
+        else
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit);
+            Instantiate(rayCastHit, hit.point, Quaternion.identity);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            
+
+        }
+    }
+}
