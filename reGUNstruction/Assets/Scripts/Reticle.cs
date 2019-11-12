dfe3efbeca983720d68a74af9cb3efacdd937513
Dominique Hill <50179031+mzhill08@users.noreply.github.com>
2019-11-11 19:39:00 -0500

    Adding in the prefabs for the two different crosshair types for the gun.
    created a static crosshair and a dynamic crosshair. also deleted extra prefab folder and placed the door prefab and the second prefab folder.

---
+++ reGUNstruction/Assets/Scripts/Reticle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reticle : MonoBehaviour
+{
+    private RectTransform reticle;
+    public float maxSize;
+    public float currentSize;
+    public float speed;
+    public float restingSize;
+
+
+    private void Start()
+    {
+        reticle = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            currentSize = Mathf.Lerp(currentSize, maxSize, Time.deltaTime * speed);
+        }
+        else
+        {
+            currentSize = Mathf.Lerp(currentSize, restingSize, Time.deltaTime * speed);
+        }
+        reticle.sizeDelta = new Vector2(currentSize, currentSize);
+    }
+
+    bool isMoving
+    {
+        get
+        {
+            if (Input.GetAxis("Mouse X") != 0 ||
+                Input.GetAxis("Mouse Y") != 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
