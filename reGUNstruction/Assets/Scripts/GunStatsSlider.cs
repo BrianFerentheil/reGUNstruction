7a55eb99bd818d43749a58307197f86c552e7329
Chenfeng Zhang <akupaqi123@gmail.com>
2019-11-18 19:33:07 -0500

    Added slider color change base on type

--- reGUNstruction/Assets/Scripts/GunStatsSlider.cs
+++ reGUNstruction/Assets/Scripts/GunStatsSlider.cs
@@ -8,23 +8,68 @@ public class GunStatsSlider : MonoBehaviour
     GunModel gunModel;
 
     public Slider damage;
-    public Slider accuracy;
-    public Slider recoil;
-    public Slider durability;
+    public GameObject damageFill;
     public Slider fireRate;
+    public GameObject fireRateFill;
+
+    Weapon weapon;
+    Color damageColor;
 
     void Start()
     {
         gunModel = FindObjectOfType<GunModel>();
+        weapon = FindObjectOfType<Weapon>();
     }
 
     
     void Update()
     {
+        UpdateSlider();
+    }
+
+    public void UpdateSlider()
+    {
         damage.value = gunModel.myStats.damage;
-        accuracy.value = gunModel.myStats.accuracy;
-        recoil.value = gunModel.myStats.recoil;
-        durability.value = gunModel.myStats.durability;
+        switch (weapon.dRng)
+        {
+            case GunModel.damageRange.low:
+                damageColor = Color.green;
+                break;
+            case GunModel.damageRange.med:
+                damageColor = Color.yellow;
+                break;
+            case GunModel.damageRange.high:
+                damageColor = Color.red;
+                break;
+            case GunModel.damageRange.none:
+                damageColor = Color.clear;
+                break;
+            default:
+                break;
+        }
+        damageFill.GetComponent<Image>().color = damageColor;
+
+
+
+
         fireRate.value = gunModel.myStats.fireRate;
+        
+
+        switch (weapon.bSpd)
+        {
+            case GunModel.bulletSpeed.low:
+                fireRateFill.GetComponent<Image>().color = Color.green;
+                break;
+            case GunModel.bulletSpeed.med:
+                fireRateFill.GetComponent<Image>().color = Color.yellow;
+                break;
+            case GunModel.bulletSpeed.fast:
+                fireRateFill.GetComponent<Image>().color = Color.red;
+                break;
+            case GunModel.bulletSpeed.none:
+                break;
+            default:
+                break;
+        }
     }
 }
