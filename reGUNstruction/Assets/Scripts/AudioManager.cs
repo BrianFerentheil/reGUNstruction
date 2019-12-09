7a8de0829bc130c95f339851b23497ca214aa74d
Chenfeng Zhang <akupaqi123@gmail.com>
2019-12-08 21:46:54 -0500

    Audio manger

---
+++ reGUNstruction/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    public List<AudioClip> clips;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        audioSource.Play();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void PlayAudio(string clipToPlay)
+    {
+        foreach(AudioClip clip in clips)
+        {
+            if(clip.name == clipToPlay)
+            {
+                audioSource.PlayOneShot(clip);
+            }
+        }
+    }
+}
