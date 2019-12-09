277f1b163d770ba201151ff547f7bd26c7dc813a
Primezurker <codyray1226@gmail.com>
2019-12-09 15:03:12 -0500

    Pause menu update
    intigrated the option menu into the pause menu. it is ready to be intigrated into scene

---
+++ reGUNstruction/Assets/Scenes/UIMenus/OptionMenuUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionMenuUI : MonoBehaviour
+{
+    public static bool OptionIsEnabled = false;
+    public GameObject optionMenuUI;
+
+// Update is called once per frame
+
+    public void Enable()
+    {
+        optionMenuUI.SetActive(false);
+        OptionIsEnabled = false;
+    }
+    public void Disable()
+    {
+        optionMenuUI.SetActive(true);
+        OptionIsEnabled = true;
+    }
+}
+
