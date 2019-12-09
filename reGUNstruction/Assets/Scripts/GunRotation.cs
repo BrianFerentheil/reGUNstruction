e9eea7e35628148dd1665a945f0e2b67ff63ea57
Dominique Hill <50179031+mzhill08@users.noreply.github.com>
2019-12-08 20:21:21 -0500

    Rotating the gun when in the workbence
    Added the rotate obj script to the gun model so that when in the workbence the player can rotate the gun if press and holding the left mouse button.

---
+++ reGUNstruction/Assets/Scripts/GunRotation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+
+    public float RotateAmount = 1;
+    public float RotateSpeed = 2;
+    public GameObject GUN;
+    public float RotateOnX;
+    public float RotateOnY;
+    public Quaternion DefaultRot;
+    public Quaternion NewGunRot;
+
+    float horizontalSpeed = 5.0f;
+    float verticalSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //DefaultRot = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateOnX += Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * RotateAmount;
+        RotateOnY += Input.GetAxis("Mouse Y") * Time.deltaTime * RotateAmount;
+        //NewGunRot = new Quaternion(DefaultRot.x + RotateOnX, DefaultRot.y + RotateOnY, DefaultRot.z);
+        NewGunRot = Quaternion.Euler(DefaultRot.x + RotateOnX, DefaultRot.y + RotateOnY, DefaultRot.z);
+        GUN.transform.localRotation = Quaternion.Lerp(GUN.transform.localRotation, NewGunRot, RotateSpeed * Time.deltaTime);
+
+
+        // Get the mouse delta. This is not in the range -1...1
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(v, h, 0);
+
+
+
+    }
+}
