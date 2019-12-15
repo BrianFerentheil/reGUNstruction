09903930adc6a92c35cff01544e71ab94f0930c1
Chenfeng Zhang <akupaqi123@gmail.com>
2019-12-14 21:30:53 -0500

    Revert "Updated NukeTown"
    Added mini game prefabs.

---
+++ reGUNstruction/Assets/Scripts/Mini-Game/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Goal>() != null)
+        {
+            other.GetComponent<Goal>().Score();
+            Destroy(gameObject,0.1f);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
---
+++ reGUNstruction/Assets/Scripts/Mini-Game/Goal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    ScoreManager sm;
+    public float point;
+
+    void Start()
+    {
+        sm = FindObjectOfType<ScoreManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Score()
+    {
+        sm.currentScore = sm.currentScore + point;
+    }
+}
