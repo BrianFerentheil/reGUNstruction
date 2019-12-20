ec5cc5ba499372301fd5ba17c91bfced4c615aab
Chenfeng Zhang <akupaqi123@gmail.com>
2019-12-20 10:33:08 -0500

    Sound effects

---
+++ reGUNstruction/Assets/Scripts/GunShooting/GunAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunAudio : MonoBehaviour
+{
+    AudioManager am;
+    GameObject gun;
+    string gunName;
+
+    void Start()
+    {
+        am = FindObjectOfType<AudioManager>();
+        gun = GameObject.FindGameObjectWithTag("Gun");
+        gunName = gun.name;
+        am.PlayAudio(gunName);
+        Debug.Log(gunName);
+    }
+}
